7c6c2fe3b7cf7c40bf6538a4f0a27e899f310f65
Luciano Krebs <lkrebs05@gmail.com>
2017-12-16 17:28:31 -0200

    improving the algorithm

--- interactive_dictionary/app_one.py
+++ interactive_dictionary/app_one.py
@@ -14,7 +14,39 @@ def translate(word):
             print("{}. {}".format(count, definition))
             count += 1
         return ""
+    elif word.capitalize() in data:
+        print("")
 
+        for definition in data[word.capitalize()]:
+            print("{}. {}".format(count, definition))
+            count += 1
+        return ""
+    elif word.upper() in data:
+        print("")
+
+        for definition in data[word.upper()]:
+            print("{}. {}".format(count, definition))
+            count += 1
+        return ""
+    elif get_close_matches(word.capitalize(), data.keys()):
+            closest_word = get_close_matches(word, data.keys())[0]
+            user_answer = (input("Did you mean {}? Enter Y if yes or N if no: ".format(closest_word))).lower()
+
+            print("")
+
+            while user_answer != "y" or user_answer != "n":
+                if user_answer == "y":
+                    for definition in data[closest_word]:
+                        print("{}. {}".format(count, definition))
+                        count += 1
+                    return ""
+                elif user_answer == "n":
+                    print("We didn't find it. Let's try again.")
+                    word = input("Enter a word: ")
+                    return translate(word)
+                else:
+                    print("We didn't understand your answer.")
+                    user_answer = (input("Did you mean {}? Enter Y if yes or N if no: ".format(closest_word))).lower()
     elif get_close_matches(word, data.keys()):
         closest_word = get_close_matches(word, data.keys())[0]
         user_answer = (input("Did you mean {}? Enter Y if yes or N if no: ".format(closest_word))).lower()
@@ -28,7 +60,9 @@ def translate(word):
                     count += 1
                 return ""
             elif user_answer == "n":
-                return "The word doesn't exist. Please double check it."
+                print("We didn't find it. Let's try again.")
+                word = input("Enter a word: ")
+                return translate(word)
             else:
                 print("We didn't understand your answer.")
                 user_answer = (input("Did you mean {}? Enter Y if yes or N if no: ".format(closest_word))).lower()
